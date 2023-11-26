4be710b228ce9b5d1c068fdabf6d7b2a4e741426
Sayantan Patra <68266225+sayantancodex@users.noreply.github.com>
2023-11-26 13:25:19 +0530

    Update q7.java

--- Assignment 2/commandline programs/q7.java
+++ Assignment 2/commandline programs/q7.java
@@ -3,12 +3,8 @@ public class q7 {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
-        if ((a <= b * c) || (b <= a * c) || (c <= a * b)){
-            System.out.println("True");
-        }
-        else{
-        System.out.println("False");
-        }
+        boolean d = ((a <= b * c) || (b <= a * c) || (c <= a * b));
+        System.out.println(d);
     }
     
 }
